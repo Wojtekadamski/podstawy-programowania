4eb9dcb26d5a795fcdfbc6d4f21e61d06c2b05f2
wojtek <wwadamski@gmail.com>
2017-12-28 15:30:11 +0100

    naprawiono pętle pesel

---
+++ Pracownik Uczelni/Pracownik Uczelni/src/GUI/interfejs.java
@@ -0,0 +1,33 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class interfejs {
+    private JPanel headder;
+    private JPanel Wybór;
+    private JButton dodaj_pracownika;
+    private JButton znajdz;
+    private JButton wyswietlAll;
+    private JButton eksportujDo;
+    private JButton zapisziZakoncz;
+    private JButton dodajStudenta;
+    private JButton zakoncz;
+    private JButton zaimportuj;
+    private JButton usun_pracownika;
+
+    public interfejs() {
+
+        dodaj_pracownika.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ Pracownik Uczelni/Pracownik Uczelni/src/GUI/mainframe.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class mainframe {
+
+    JFrame ramka = new JFrame();
+    JButton przycisk = new JButton();
+
+
+}
